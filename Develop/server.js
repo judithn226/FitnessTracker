fac5972d39a3264aedcae59b9d4f1ced29acb435
Judith Nwagwu <Judith5nwagwu@gmail.com>
2020-12-31 11:50:19 -0800

    updating server.js

---
+++ Develop/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const db = require("./models");
+const app = express();
+const path = require("path");
+
+app.use(logger("dev"));
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(`${__dirname}/public/exercise.html`));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(`${__dirname}/public/stats.html`));
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+require("./routes/routes.js")(app);
+
+const PORT = process.env.PORT || 3000;
+
+const DeprecationWarning = {
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+   
+    
+};
+// for mongoose to heroku
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", DeprecationWarning)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`localhost:${PORT}`);
+        });
+    });
